c9a7d7b189d0d1fae522f967256221f196f301db
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-01 09:14:25 -0400

    added more players, added game start time

--- studio-game.rb
+++ studio-game.rb
@@ -1,11 +1,20 @@
+time = Time.new
+formatted_time = time.strftime("%A %m/%d/%Y at %I:%M%p")
+puts "\nThe game started on #{formatted_time}.\n\n"
+
 name1 = "larry"
 health1 = 60
-puts "#{name1}'s health is #{health1}."
+puts "#{name1.capitalize} has a health of #{health1}."
 
 name2 = "curly"
-health2 = 60
-puts "#{name2}'s health is #{health2}."
+health2 = 125
+puts "#{name2.upcase} has a health of #{health2}."
 
 name3 = "moe"
-health3 = 60
-puts "#{name3}'s health is #{health3}."
+health3 = 100
+puts "#{name3.capitalize} has a health of #{health3}.".center(50, '*')
+
+name4 = "shemp"
+health4 = 90
+puts "#{name4.capitalize.ljust(30, '.')} #{health4} health"
+
