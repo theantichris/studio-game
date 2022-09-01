25478fa09d7d2ce1bcdde58520f64c91e287414b
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-01 08:00:15 -0400

    added player2 and player3

--- studio-game.rb
+++ studio-game.rb
@@ -1,3 +1,11 @@
 name1 = "larry"
 health1 = 60
 puts "#{name1}'s health is #{health1}."
+
+name2 = "curly"
+health2 = 60
+puts "#{name2}'s health is #{health2}."
+
+name3 = "moe"
+health3 = 60
+puts "#{name3}'s health is #{health3}."
