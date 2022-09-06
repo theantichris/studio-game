9ba08f0a7555d1ffb9de66f9262663b645a29192
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-06 13:31:07 -0400

    added example code

--- player.rb
+++ player.rb
@@ -29,3 +29,15 @@ class Player
     @health + @name.length
   end
 end
+
+if __FILE__ == $0
+  player = Player.new("moe")
+  puts player.name
+  puts player.health
+
+  player.w00t
+  puts player.health
+
+  player.blam
+  puts player.health
+end
