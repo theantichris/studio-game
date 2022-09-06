39c4ec52847466115768a1f4824467603bbded35
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-06 12:43:42 -0400

    cleanup

--- studio-game.rb
+++ studio-game.rb
@@ -33,27 +33,7 @@ end
 player1 = Player.new("moe")
 player2 = Player.new("larry", 60)
 player3 = Player.new("curly", 125)
-
 players = [player1, player2, player3]
-puts "There are #{players.size} players in the game:"
-players.each do |player|
-  puts player
-end
-
-players.each do |player|
-  puts player.health
-end
-
-players.each do |player|
-  player.blam
-  player.w00t
-  player.w00t
-  puts player
-end
-
-players.pop
-player4 = Player.new("shemp", 90)
-players.push(player4)
 
 puts "There are #{players.size} players in the game:"
 players.each do |player|
