9b66df68ba66107274f571020e76a1bd6538d845
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-07 13:16:59 -0400

    added GameTurn module

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require_relative 'player'
 require_relative 'die'
+require_relative 'game_turn'
 
 class Game
   attr_reader :title
@@ -24,17 +25,7 @@ class Game
     puts "\n\n"
 
     @players.each do |player|
-      die = Die.new
-      number_rolled = die.roll
-
-      case number_rolled
-      when 1..2
-        player.blam
-      when 3..4
-        puts "#{player.name} was skipped."
-      else
-        player.w00t
-      end
+      GameTurn.take_turn(player)
 
       puts "#{player}\n\n"
     end
