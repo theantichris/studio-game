de9a91fcd24555cd11cb387ebb6b4e8b6bea5f50
Christopher Lamm <christopher.lamm@companycam.com>
2022-09-12 08:51:26 -0400

    updated game stats for new score

--- game.rb
+++ game.rb
@@ -57,10 +57,15 @@ class Game
       print_name_and_health(player)
     end
 
+    @players.each do |player|
+      puts "\n#{player.name}'s point totals:"
+      puts "#{player.points} grand total points"
+    end
+
     puts "\n#{@title} High Scores:"
     sorted_players = @players.sort { |a, b| b.score <=> a.score }
     sorted_players.each do |player|
-      puts "#{player.name.ljust(20, '.')} #{player.health}"
+      puts "#{player.name.ljust(20, '.')} #{player.score}"
     end
   end
 end
